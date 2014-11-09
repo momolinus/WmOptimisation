3d4b3cd1efe8d05ad34b7b036f5b00133b75cdd1
Marcus <m.bleil@gmx.net>
2014-11-09 12:25:50 +0100

    found the error-symptom: area > 0 and no changes < 1

--- Wheelmap Optimisation/Analysis-first-opt.-no_changes.R
+++ Wheelmap Optimisation/Analysis-first-opt.-no_changes.R
@@ -10,6 +10,11 @@ changesets <- read.table(file = "optimization_5.csv", header=T, dec=".", sep=";"
 # inspect the data set
 str(changesets)
 names(changesets)
+summary(changesets$user)
+summary(changesets$algorithm)
+summary(changesets$no_changes)
+summary(changesets$area)
+
 # 10945
 length(changesets$changesetId)
 
@@ -32,6 +37,13 @@ length(changesets.no_changes.is_greater_than_1$changesetId)
 # = 10945
 9732 + 912 + 301
 
+#################################
+### tables: number of changes ###
+#################################
+
+# number of changes for user/algorithm
+table(changesets$user, changesets$algorithm)
+
 # table: for each user the number of changesets for each algorithm
 # and changesets == 0
 table(changesets.no_changes.is_0$user, changesets.no_changes.is_0$algorithm)
@@ -44,23 +56,53 @@ table(changesets.no_changes.is_1$user, changesets.no_changes.is_1$algorithm)
 # and changesets > 1
 table(changesets.no_changes.is_greater_than_1$user, changesets.no_changes.is_greater_than_1$algorithm)
 
-### Inspect suspicion changesets  
-changesets.areaguard <- subset(changesets, algorithm == 'area guard (0.0073)' && user == 'wheelmap_visitor')
-length(changesets.areaguard$changesetId)
+####################################
+### Inspect suspicion changesets ###
+####################################
+ 
+changesets.wheel <- subset(changesets, algorithm == 'area guard (0.0073)' & user == 'wheelmap_visitor')
+str(changesets.wheel)
+summary(changesets.wheel$user)
+# 5878 changsets for "wheelchair_visitor" with "area guard (0.0073)"
+length(changesets.wheel$changesetId)
 
-changesets.areaguard.error <- subset (changesets.areaguard, area > 0 && no_changes < 1)
-length(changesets.areaguard.error$changesetId)
+### the wrong changesets ###
 
-changesets.areaguard.no_changes.is_0 <- subset (changesets.areaguard, no_changes == 0)
-# 5877
-length(changesets.areaguard.no_changes.is_0$changesetId)
+## area > 0 ##
 
-changesets.areaguard.no_changes.is_1 <- subset (changesets.areaguard, no_changes == 1)
+# area > 0 and no_changes < 0 ###### this must be wrong #######
+changesets.wheel.error <- subset (changesets.wheel, area > 0 & no_changes < 1)
+# 2642
+length(changesets.wheel.error$changesetId)
+
+# area > 0 and no_changes == 1 could not be
+changesets.wheel.error2 <- subset (changesets.wheel, area > 0 & no_changes == 1)
 # 0
-length(changesets.areaguard.no_changes.is_1$changesetId)
+length(changesets.wheel.error2$changesetId)
 
-changesets.areaguard.no_changes.is_greater_than_1 <- subset (changesets.areaguard, no_changes > 1)
+# area > 0 and no_changes > 1 could  be
+changesets.wheel.error3 <- subset (changesets.wheel, area > 0 & no_changes > 1)
+# 0
+length(changesets.wheel.error3$changesetId)
+
+## area <= 0 ##
+
+# the 1. rest area <= 0 and no changes < 1
+changesets.wheel.error4 <- subset (changesets.wheel, area <= 0 & no_changes < 1)
+# 3235
+length(changesets.wheel.error4$changesetId)
+
+# the 2. rest area <= 0 and no changes == 1
+changesets.wheel.error5 <- subset (changesets.wheel, area <= 0 & no_changes == 1)
+# 0
+length(changesets.wheel.error5$changesetId)
+
+# the 3. rest area <= 0 and no changes > 1 could be on the same node
+changesets.wheel.error6 <- subset (changesets.wheel, area <= 0 & no_changes > 1)
 # 1
-length(changesets.areaguard.no_changes.is_greater_than_1$changesetId)
-summary(changesets.areaguard.no_changes.is_greater_than_1)
+length(changesets.wheel.error6$changesetId)
 
+## Test the number ##
+
+# = 5878
+2642 + 3235 + 1
